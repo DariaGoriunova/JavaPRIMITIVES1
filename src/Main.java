9555915e76ece4fd51699b0974406b636a936d8e
Daria <Dasha-esha@mail.ru>
2024-10-02 16:09:44 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int startingSum = 250;
+        int addSum1 = 550;
+        int addSum2 = 2150;
+
+        int bonus;
+        if (addSum1 >= 1000) {
+            bonus = addSum1 / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int bonus1;
+        if (addSum2 >= 1000) {
+            bonus1 = addSum2 / 100;
+        } else {
+            bonus1 = 0;
+        }
+
+        System.out.println("Количество бонусов 1 клиента:" + bonus + " " + "Итоговый счет:" + (startingSum + addSum1 + bonus));
+        System.out.println("Количество бонусов 2 клиента:" + bonus1 + " " + "Итоговый счет:" + (startingSum + addSum2 + bonus1));
+
+    }
 }
